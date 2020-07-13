ed403a4dc40fd743c648e90730e645f99cff4c44
Anna Kuznetsova <annavk@fb.com>
2020-07-13 10:08:10 -0700

    added xcode files

---
+++ ClosetFriend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClosetFriend
+//
+//  Created by Anna Kuznetsova on 7/13/20.
+//  Copyright © 2020 Anna Kuznetsova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
